9a14461571f68c195dcf329823d3dc8fd4dbb3da
Zack Patrick <zpatrick319@Hotmail.com>
2018-06-25 15:18:03 -0700

    initial commit

---
+++ eks/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+  type        = "string"
+  description = "The name of the cluster and associated resources"
+  default     = "test"
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "The name of the AWS region to place resources"
+  default     = "us-west-2"
+}
