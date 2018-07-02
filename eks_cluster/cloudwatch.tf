45112a9d23ada823ff2ed2f5b7f777c29087c1c4
Nick Hayward <nehayward@gmail.com>
2018-07-02 13:16:03 -0700

    Update logging to be only local exec

--- eks_cluster/cloudwatch.tf
+++ eks_cluster/cloudwatch.tf
@@ -2,8 +2,10 @@ resource "aws_cloudwatch_log_group" "this" {
   name = "${var.cluster_name}-logs"
 
   provisioner "local-exec" {
-    command     = "make install"
-    working_dir = "scripts/"
+    command = <<EOF
+    helm repo add quintilesims 'https://raw.githubusercontent.com/quintilesims/kubernetes-fluentd-cloudwatch/master/' &&
+    helm install quintilesims/fluentd-cloudwatch --set aws.id=$(echo $AWS_ACCESS_KEY) --set aws.secret=$(echo $AWS_SECRET_KEY) --set aws.loggroup=${self.name} --name fluentd
+EOF
 
     environment {
       AWS_ACCESS_KEY = ""
