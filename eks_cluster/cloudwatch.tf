d9acf3990badcefd9e0c5c9bca9e61bd3cc97f0d
Nick Hayward <nehayward@gmail.com>
2018-07-03 10:21:29 -0700

    Clean up local exec

--- eks_cluster/cloudwatch.tf
+++ eks_cluster/cloudwatch.tf
@@ -3,8 +3,12 @@ resource "aws_cloudwatch_log_group" "this" {
 
   provisioner "local-exec" {
     command = <<EOF
-    helm repo add quintilesims 'https://raw.githubusercontent.com/quintilesims/kubernetes-fluentd-cloudwatch/master/' &&
-    helm install quintilesims/fluentd-cloudwatch --set aws.id=$(echo $AWS_ACCESS_KEY) --set aws.secret=$(echo $AWS_SECRET_KEY) --set aws.loggroup=${self.name} --name fluentd
+    helm repo add quintilesims 'https://raw.githubusercontent.com/nehayward/kubernetes-fluentd-cloudwatch/master/' &&
+    helm install quintilesims/fluentd-cloudwatch \
+    --set aws.id=$(echo $AWS_ACCESS_KEY) \
+    --set aws.secret=$(echo $AWS_SECRET_KEY) \
+    --set aws.loggroup=${self.name} \
+    --name fluentd
 EOF
 
     environment {
