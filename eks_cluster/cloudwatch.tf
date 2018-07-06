9116f17a170c9e15eaa0e0aaf5280ed3bf5ed91a
Nick Hayward <nehayward@gmail.com>
2018-07-06 09:59:59 -0700

    Move AWS ID and Secret to terraform variable instead of env variables

--- eks_cluster/cloudwatch.tf
+++ eks_cluster/cloudwatch.tf
@@ -3,17 +3,12 @@ resource "aws_cloudwatch_log_group" "this" {
 
   provisioner "local-exec" {
     command = <<EOF
-    helm repo add quintilesims 'https://raw.githubusercontent.com/nehayward/kubernetes-fluentd-cloudwatch/master/' &&
-    helm install quintilesims/fluentd-cloudwatch \
-    --set aws.id=$(echo $AWS_ACCESS_KEY) \
-    --set aws.secret=$(echo $AWS_SECRET_KEY) \
+    helm repo add iqvia 'https://raw.githubusercontent.com/nehayward/kubernetes-fluentd-cloudwatch/master/' &&
+    helm install iqvia/fluentd-cloudwatch \
+    --set aws.id=${var.aws_access_id} \
+    --set aws.secret=${var.aws_secret_access_key} \
     --set aws.loggroup=${self.name} \
     --name fluentd
 EOF
-
-    environment {
-      AWS_ACCESS_KEY_ID     = ""
-      AWS_SECRET_ACCESS_KEY = ""
-    }
   }
 }
