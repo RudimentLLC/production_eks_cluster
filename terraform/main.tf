e32e5e14120257438217824f73a5839daaf4d7ef
Tanner Lake <tanner.lake@gmail.com>
2019-03-11 13:02:22 -0700

    Fix field name typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,7 @@ locals {
       instance_type         = "t3.medium"
       additional_userdata   = "${var.additional_userdata}"
       subnets               = "${join(",", module.vpc.private_subnets)}"
-      asg_desired_count     = 2
+      asg_desired_capacity  = 2
       asg_min_size          = 2
       asg_max_size          = 10
       autoscaling_enabled   = "true"
